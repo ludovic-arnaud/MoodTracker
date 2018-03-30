46b2ff867b22b6e05c3b345a726d0bc2b7bdedec
ludovic-arnaud <36990657+ludovic-arnaud@users.noreply.github.com>
2018-03-30 11:29:12 +0200

    PageAdapter and its methods created

---
+++ app/src/main/java/com/arnaud/ludovic/moodtracker/adapters/PageAdapter.java
@@ -0,0 +1,38 @@
+package com.arnaud.ludovic.moodtracker.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.arnaud.ludovic.moodtracker.controller.MoodFragment;
+
+/**
+ * Created by Ludo on 30/03/2018.
+ */
+
+public class PageAdapter extends FragmentPagerAdapter {
+
+    //Array of colors that will be passed to MoodFragment
+    private int[] colors;
+
+    //Array of moods that will be passed to MoodFragment
+    private int[] moods;
+
+    //Default constructor
+    public PageAdapter(FragmentManager mgr, int[] colors, int[] moods){
+        super(mgr);
+        this.colors = colors;
+        this.moods = moods;
+    }
+
+    @Override
+    public int getCount() {
+        return 5; //Number of pages to show
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        //Page to return
+        return (MoodFragment.newInstance(position, this.colors[position], this.moods[position]));
+    }
+}
