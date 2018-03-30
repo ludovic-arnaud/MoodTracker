b13735addb16637b8965ef0a5821647bcb1b8bdb
ludovic-arnaud <36990657+ludovic-arnaud@users.noreply.github.com>
2018-03-30 17:00:58 +0200

    Add verticalviewpager library from castorplex in gradle build

--- app/src/main/java/com/arnaud/ludovic/moodtracker/controller/MainActivity.java
+++ app/src/main/java/com/arnaud/ludovic/moodtracker/controller/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void configureViewPager(){
         //Array of moods
-        final int[] moods = {R.drawable.smiley_disappointed, R.drawable.smiley_happy, R.drawable.smiley_normal, R.drawable.smiley_sad, R.drawable.smiley_super_happy};
+        final int[] moods = {R.drawable.smiley_sad, R.drawable.smiley_disappointed, R.drawable.smiley_normal, R.drawable.smiley_happy, R.drawable.smiley_super_happy};
 
         //Get ViewPages from Layout
         ViewPager pager = (ViewPager) findViewById(R.id.activity_main_viewpager);
