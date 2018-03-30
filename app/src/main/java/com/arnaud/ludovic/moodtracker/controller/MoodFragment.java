3ae9c405d70f0e5a8195c84262952613fcb16a21
ludovic-arnaud <36990657+ludovic-arnaud@users.noreply.github.com>
2018-03-30 11:20:54 +0200

    Move MoodFragment to controller folder

--- app/src/main/java/com/arnaud/ludovic/moodtracker/controller/MoodFragment.java
+++ app/src/main/java/com/arnaud/ludovic/moodtracker/controller/MoodFragment.java
@@ -1,4 +1,4 @@
-package com.arnaud.ludovic.moodtracker;
+package com.arnaud.ludovic.moodtracker.controller;
 
 
 import android.os.Bundle;
@@ -8,6 +8,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+
+import com.arnaud.ludovic.moodtracker.R;
 
 
 /**
