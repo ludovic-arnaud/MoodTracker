8e5ee11f94b448db0800f96d6dbe2c0975616665
ludovic-arnaud <36990657+ludovic-arnaud@users.noreply.github.com>
2018-03-30 17:23:37 +0200

    Delete verticalviewpager library from castorplex do not work

--- app/src/main/java/com/arnaud/ludovic/moodtracker/controller/MainActivity.java
+++ app/src/main/java/com/arnaud/ludovic/moodtracker/controller/MainActivity.java
@@ -1,16 +1,14 @@
 package com.arnaud.ludovic.moodtracker.controller;
 
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewParent;
 
 import com.arnaud.ludovic.moodtracker.R;
 import com.arnaud.ludovic.moodtracker.adapters.PageAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         final int[] moods = {R.drawable.smiley_sad, R.drawable.smiley_disappointed, R.drawable.smiley_normal, R.drawable.smiley_happy, R.drawable.smiley_super_happy};
 
         //Get ViewPages from Layout
-        ViewPager pager = (ViewPager) findViewById(R.id.activity_main_viewpager);
+        ViewPager pager = findViewById(R.id.activity_main_viewpager);
         //Set adapter PageAdapter and glue it together
         pager.setAdapter(new PageAdapter(
                 getSupportFragmentManager(),
