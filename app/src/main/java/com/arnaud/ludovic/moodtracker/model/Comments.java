a72d7f7b83494204dbe2192b3fae8f96660e1f24
ludovic-arnaud <36990657+ludovic-arnaud@users.noreply.github.com>
2018-03-31 10:04:52 +0200

    Create model/Comments.java

---
+++ app/src/main/java/com/arnaud/ludovic/moodtracker/model/Comments.java
@@ -0,0 +1,20 @@
+package com.arnaud.ludovic.moodtracker.model;
+
+/**
+ * Created by Ludo on 31/03/2018.
+ */
+
+public class Comments {
+
+    private String mComments;
+
+    //Getter
+    public String getComments() {
+        return mComments;
+    }
+
+    //Setter
+    public void setComments(String comments) {
+        mComments = comments;
+    }
+}
